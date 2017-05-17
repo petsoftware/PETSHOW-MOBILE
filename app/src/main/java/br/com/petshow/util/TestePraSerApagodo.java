ecc98983e0fe7948af04137cdddfbc96750930e9
bruno <bruno_salmito@hotmail.com>
2017-05-17 11:29:05 -0300

    Teste para verificar o Commit

--- app/src/main/java/br/com/petshow/util/TestePraSerApagodo.java
+++
@@ -1,9 +0,0 @@
-package br.com.petshow.util;
-
-/**
- * Classe de teste feita no PC do Bruno. Deve ser apagada ao final.
- * Created by bruno on 17/05/2017.
- */
-
-public class TestePraSerApagodo {
-}
