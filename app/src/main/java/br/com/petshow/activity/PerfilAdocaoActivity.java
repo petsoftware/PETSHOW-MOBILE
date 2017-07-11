b7f5bed09e4952a573b84de3dae97ca96d0e70f2
rafasystec <rafasystec@yahoo.com.br>
2017-07-10 23:11:44 -0300

    Tela de perfil de adoção android

--- app/src/main/java/br/com/petshow/activity/PerfilAdocaoActivity.java
+++ app/src/main/java/br/com/petshow/activity/PerfilAdocaoActivity.java
@@ -2,30 +2,17 @@ package br.com.petshow.activity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.Spinner;
-
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-
 import br.com.petshow.R;
 import br.com.petshow.enums.EnumFaseVida;
-import br.com.petshow.enums.EnumFrequenciaTratamento;
 import br.com.petshow.enums.EnumPorteAnimal;
 import br.com.petshow.enums.EnumTipoAnimal;
 import br.com.petshow.model.PerfilAdocao;
-import br.com.petshow.model.Tratamento;
-import br.com.petshow.util.DateUtilsAndroid;
 import br.com.petshow.util.JsonUtil;
 import br.com.petshow.util.MapErroRetornoRest;
 import br.com.petshow.view.util.CriationUtil;
@@ -38,10 +25,15 @@ import br.com.petshow.web.util.RequestPostEntity;
 
 public class PerfilAdocaoActivity extends PetActivity {
 
+    //Declaring the basic components
     Spinner spTipo;
     Spinner spFase;
     Spinner spPorte;
-
+    //Declaring all used Adapters
+    ArrayAdapter<EnumFaseVida> faseVidaAdapter;
+    ArrayAdapter<EnumPorteAnimal> porteAnimalAdapter;
+    ArrayAdapter<EnumTipoAnimal> tipoAnimalAdapter;
+    //Object model from DataBase by REST
     PerfilAdocao perfilAdocao;
 
     @Override
@@ -52,13 +44,12 @@ public class PerfilAdocaoActivity extends PetActivity {
         startComponents();
         startVariables();
         if(perfilAdocao!=null  && perfilAdocao.getId()>0) {
-            loadPerfil();
             if(perfilAdocao==null){
                 loadSpinnersValuesDefault();
             }
         }else{
           //Preencher
-            //loadSpinners(null);
+
         }
     }
     public void startComponents() {
@@ -108,59 +99,32 @@ public class PerfilAdocaoActivity extends PetActivity {
     }
 
     public void startVariables(){
-        perfilAdocao= perfilAdocao==null?new PerfilAdocao():perfilAdocao;
-
+        //Initializing variable
+        perfilAdocao        = perfilAdocao==null?new PerfilAdocao():perfilAdocao;
+        faseVidaAdapter     = new ArrayAdapter<EnumFaseVida>(this,R.layout.support_simple_spinner_dropdown_item,EnumFaseVida.values());
+        tipoAnimalAdapter   = new ArrayAdapter<EnumTipoAnimal>(this,R.layout.support_simple_spinner_dropdown_item,EnumTipoAnimal.values());
+        porteAnimalAdapter  = new ArrayAdapter<EnumPorteAnimal>(this,R.layout.support_simple_spinner_dropdown_item,EnumPorteAnimal.values());
     }
 
     //Utilizaremos este metodo para buscar e preencher spnners de acordo com o banco de dados
 
-    private void loadSpinnerTipo(String tipo){
-        //No caso nos chamamos o RequestListObject pois queremos chamar um REST q vai retornar uma lista
-        //porem existem outros que devemos chamar de acordo com o q gostariamos de fazer
-        //ver mais detalhes no pacote web.util
-        new RequestListObjects(new CallBack(this) {
-            @Override
-            public void successWithReturn(String json) {
-                List<String> lista=null;
-                try {
-                    //Pegando a resposta do REST que veio como parametro
-                    lista= JsonUtil.jsonToList(new String(), json,String.class);
-                    //Adicionado o "Selecione" como default para ser o primeiro
-                    lista.add(0, getContext().getString(R.string.selAnimal));
-                    //Prenche o spinner de acordo com os parametros.
-                    CriationUtil.createArrayAdapter(this.getContext(),spTipo, lista);
-                    // Aqui iremos preencher o spinner
-                    if(tipo !=null){
-                        for(int i=0;lista.size() >i;++i){
+    private void loadSpinners(PerfilAdocao perfilAdocao){
 
-                            if(lista.get(i).equals(tipo)){
-                                spTipo.setSelection(i);
-                                //Caso encontre a apçãol desejada , iremos parar
-                                break;
-                            }
-                        }
+       if(perfilAdocao!=null){
+           if(perfilAdocao.getFaseVida() != null){
+               int positionFaseVida = faseVidaAdapter.getPosition(perfilAdocao.getFaseVida());
+               spFase.setSelection(positionFaseVida);
+           }
+           if(perfilAdocao.getPorteAnimal() != null){
+               int positionPorteAnimal = porteAnimalAdapter.getPosition(perfilAdocao.getPorteAnimal());
+               spPorte.setSelection(positionPorteAnimal);
+           }
+           if(perfilAdocao.getTipoAnimal() != null){
+               int positionTipoAnimal = tipoAnimalAdapter.getPosition(perfilAdocao.getTipoAnimal());
+               spTipo.setSelection(positionTipoAnimal);
+           }
+       }
 
-                    }
-
-
-
-                } catch (Exception e) {
-                    //Em caso de exceção sempre chamar esse metodo.
-                    MessageUtil.messageErro(getContext(),e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void successNoReturn() {
-
-            }
-
-            @Override
-            public void predictedError(MapErroRetornoRest map) {
-
-            }
-        }).execute("animal/consulta/tipos");
     }
 
     public void  salvar(){
@@ -190,10 +154,7 @@ public class PerfilAdocaoActivity extends PetActivity {
         }).execute("url do REST",perfilAdocao);
     }
 
-    private void loadPerfil(){
-        //TODO Carregar os valores detro dos componentes
-        loadSpinnerTipo(perfilAdocao.getTipoAnimal().toString());
-    }
+
 
     private void onClickSalvar(){
         //TODO Por metodo para savar, aproveitar e por as validações.
@@ -210,8 +171,8 @@ public class PerfilAdocaoActivity extends PetActivity {
     private void loadSpinnersValuesDefault(){
     //spFaixaIdade.setAdapter(new ArrayAdapter<EnumFaseVida>(this, android.R.layout.simple_list_item_1, EnumFaseVida.values()));
 
-        spTipo.setAdapter(new ArrayAdapter<EnumTipoAnimal>(this,R.layout.support_simple_spinner_dropdown_item,EnumTipoAnimal.values()));
-        spFase.setAdapter(new ArrayAdapter<EnumFaseVida>(this,R.layout.support_simple_spinner_dropdown_item, EnumFaseVida.values()));
-        spPorte.setAdapter(new ArrayAdapter<EnumPorteAnimal>(this,R.layout.support_simple_spinner_dropdown_item, EnumPorteAnimal.values()));
+        spTipo.setAdapter(tipoAnimalAdapter);
+        spFase.setAdapter(faseVidaAdapter);
+        spPorte.setAdapter(porteAnimalAdapter);
     }
 }
